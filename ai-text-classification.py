3fce56a95de784fc6b8e32ac6c685772dc750a7a
Brian Salchert <brisalchert@gmail.com>
2024-09-21 12:13:39 -0400

    Import numpy and pandas

---
+++ ai-text-classification.py
@@ -0,0 +1,9 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# AI Text Classification
+#
+# Python script for training a CNN for classifying AI-generated versus human-generated text.
+#-----------------------------------------------------------------------------------------------------------------------
+
+# Import packages
+import numpy as np
+import pandas as pd
