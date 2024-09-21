e7c3a0c3e67053fb8c6442b752ccfc606701822a
Brian Salchert <brisalchert@gmail.com>
2024-09-21 13:03:38 -0400

    Preview data visually with seaborn countplot

--- ai-text-classification.py
+++ ai-text-classification.py
@@ -7,9 +7,20 @@
 # Import packages
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import torch
 
 # Load dataset from csv
 ai_human_df = pd.read_csv('ai_human.csv')
 
 # Preview data
 print(ai_human_df.head())
+print(ai_human_df.info())
+
+# Visualize distribution of data
+sns.countplot(x=ai_human_df['generated'])
+plt.title('Class Distribution')
+plt.xlabel('Class')
+plt.ylabel('Count')
+plt.show()
