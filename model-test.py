4449e51a0d0a23b5e9b767ac54842cf27b8dc12f
Brian Salchert <brisalchert@gmail.com>
2024-11-01 19:53:48 -0400

    Add ROC curve plot for evaluating test performance
    Area under the curve helps to measure model performance. Additionally, the points on the curve help to identify the best possible prediction threshold for classification.

--- model-test.py
+++ model-test.py
@@ -1,6 +1,9 @@
+import numpy as np
 import torch
 import pickle
 import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.metrics import roc_curve, roc_auc_score
 from tqdm import tqdm
 from essayLSTM import EssayLSTM
 from preprocessing import EssayPreprocessor
@@ -39,7 +42,7 @@ num_layers = model_params['num_layers']
 device = 'cpu'
 
 # Set prediction threshold
-threshold = 0.7
+threshold = 0.999849
 
 # Load the model
 model = EssayLSTM(vocab_size, embed_size, hidden_size, num_layers, device)
@@ -47,16 +50,19 @@ model.load_state_dict(torch.load('ai-text-model.pt', weights_only=True))
 model.eval()
 model.to('cpu')
 
-def predict(essay, essay_pipeline, threshold):
+def predict(essay, essay_pipeline):
     with torch.no_grad():
         text = essay_pipeline(essay)
         text = torch.tensor(text)
+
+        if len(text) == 0:
+            raise ValueError("Invalid input for prediction.")
+
         sequence_length = torch.tensor(text.shape)
         # Reshape text for batch-first tensor format
         text = text.reshape(1, -1)
         output = model(text, sequence_length)
-        predicted_label = (torch.sigmoid(output) >= threshold).int().item()
-        return predicted_label
+        return output
 
 gen_essay_label = {0: 'human-generated', 1: 'AI-generated'}
 
@@ -65,21 +71,56 @@ true_pos, true_neg = 0, 0
 count_human, count_ai = 0, 0
 count_total = len(sample_essays)
 
-print("Testing sample:")
+labels = []
+pred_labels = []
+
+for i in range(len(sample_essays)):
+    try:
+        pred_label = torch.sigmoid(predict(sample_essays.iloc[i,0], preprocessor.essay_processing_pipeline)).item()
+        pred_labels.append(pred_label)
+
+        label = sample_essays.iloc[i, 1]
+        labels.append(label)
+    # Ignore samples that are invalid for predictions
+    except ValueError:
+        pass
+
+np.array(labels)
+np.array(pred_labels)
+fpr, tpr, thresholds = roc_curve(labels, pred_labels)
+au_roc = roc_auc_score(labels, pred_labels)
+
+roc_df = pd.DataFrame({"fpr": fpr, "tpr": tpr, "threshold": thresholds})
+roc_df['cutoff'] = tpr - fpr
+
+print(roc_df.sort_values("cutoff", ascending=False).head())
+print("AU ROC score: ", au_roc)
+
+plt.plot(fpr, tpr, marker='.')
+plt.title("ROC Curve for Testing Data")
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.show()
+
 for i in tqdm(range(len(sample_essays))):
-    true_label = gen_essay_label[sample_essays.iloc[i, 1]]
-    pred_label = gen_essay_label[predict(sample_essays.iloc[i,0], preprocessor.essay_processing_pipeline, threshold)]
+    try:
+        true_label = gen_essay_label[sample_essays.iloc[i, 1]]
+        pred_label = gen_essay_label[(torch.sigmoid(
+            predict(sample_essays.iloc[i,0],preprocessor.essay_processing_pipeline)
+        ) >= threshold).int().item()]
 
-    if true_label == pred_label:
-        if true_label == 'AI-generated':
-            true_pos += 1
+        if true_label == pred_label:
+            if true_label == 'AI-generated':
+                true_pos += 1
+            else:
+                true_neg += 1
+
+        if pred_label == gen_essay_label[0]:
+            count_human += 1
         else:
-            true_neg += 1
-
-    if pred_label == gen_essay_label[0]:
-        count_human += 1
-    else:
-        count_ai += 1
+            count_ai += 1
+    except ValueError:
+        pass
 
 count_correct = true_pos + true_neg
 valid_accuracy = count_correct / count_total
