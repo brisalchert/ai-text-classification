fc8afe5d351edf5084654fedb2fe39ec1a739c17
Brian Salchert <brisalchert@gmail.com>
2024-11-02 14:48:30 -0400

    Remove punctuation and emojis from input

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,16 @@
 import re
 
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
 def get_tokenizer():
     """
     Gets a tokenizer function for basic english sentences. Adapted from the deprecated TorchText library.
@@ -8,7 +19,7 @@ def get_tokenizer():
 
     _patterns = [r"\'", r"\"", r"\.", r"<br \/>", r",", r"\(", r"\)", r"\!", r"\?", r"\;", r"\:", r"\s+"]
 
-    _replacements = [" '  ", "", " . ", " ", " , ", " ( ", " ) ", " ! ", " ? ", " ", " ", " "]
+    _replacements = ["", "", "", " ", "", "", "", "", "", " ", " ", " "]
 
     _patterns_dict = list((re.compile(p), r) for p, r in zip(_patterns, _replacements))
 
@@ -21,6 +32,7 @@ def get_tokenizer():
         line = line.lower()
         for pattern_re, replaced_str in _patterns_dict:
             line = pattern_re.sub(replaced_str, line)
+        line = remove_emoji(line)
         return line.split()
 
     return _basic_english_normalize
