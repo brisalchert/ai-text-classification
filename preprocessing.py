4420d4bcbac41fe561a0a6612df34fa3461fec96
Brian Salchert <brisalchert@gmail.com>
2024-11-02 14:48:45 -0400

    Replace stemming with lemmatization

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,15 @@
 from tokenizer import get_tokenizer
-from nltk import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 import nltk
 from nltk.corpus import stopwords
 nltk.download("stopwords")
+nltk.download("wordnet")
 
 class EssayPreprocessor:
     def __init__(self, vocab):
         self.tokenizer = get_tokenizer()
         self.vocab = vocab
-        self.stemmer = PorterStemmer()
+        self.lemma = WordNetLemmatizer()
         self.stop_words = set(stopwords.words("english"))
 
     def generate_tokens(self, essay):
@@ -16,8 +17,8 @@ class EssayPreprocessor:
         tokens = self.tokenizer(essay)
         # Remove stopwords
         tokens = [token for token in tokens if token not in self.stop_words]
-        # Stem remaining words
-        tokens = [self.stemmer.stem(token) for token in tokens]
+        # Lemmatize remaining words
+        tokens = [self.lemma.lemmatize(token) for token in tokens]
         return tokens
 
     def essay_processing_pipeline(self, essay):
